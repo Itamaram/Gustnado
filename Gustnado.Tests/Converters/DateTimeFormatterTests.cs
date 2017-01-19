25bf7ea3f3c2458967e0f1171582a10c54b6a099
Itamar Amith <itamaram@gmail.com>
2017-01-20 07:55:03 +1100

    post rebase fix

--- Gustnado.Tests/Converters/DateTimeFormatterTests.cs
+++ Gustnado.Tests/Converters/DateTimeFormatterTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using Gustnado.Converters;
-using Gustnado.Enums;
-using Gustnado.Objects;
-using Newtonsoft.Json;
 using NUnit.Framework;
 
 namespace Gustnado.Tests.Converters
@@ -19,31 +16,6 @@ namespace Gustnado.Tests.Converters
         public void FormattingTests(int year, int month, int day, int hour, int minute, int second, string formatted)
         {
             Assert.AreEqual(formatted, DateTimeFormatter.Format(new DateTime(year, month, day, hour, minute, second, DateTimeKind.Utc)));
-        }
-    }
-
-    public class EnumConverterTests
-    {
-        [TestCase("processing", EncodingState.Processing)]
-        [TestCase("failed", EncodingState.Failed)]
-        [TestCase("finished", EncodingState.Finished)]
-        public void FromJson(string json, EncodingState expected)
-        {
-            Assert.AreEqual(expected, JsonConvert.DeserializeObject<Foo>($"{{ State: \"{json}\"}}").State);
-        }
-
-        [TestCase(EncodingState.Processing, "processing")]
-        [TestCase(EncodingState.Failed, "failed")]
-        [TestCase(EncodingState.Finished, "finished")]
-        public void ToJson(EncodingState state, string expected)
-        {
-            Assert.AreEqual($"{{\"State\":\"{expected}\"}}", JsonConvert.SerializeObject(new Foo { State = state }));
-        }
-
-        private class Foo
-        {
-            [JsonConverter(typeof(EnumConverter<EncodingState>))]
-            public EncodingState State { get; set; }
         }
     }
 }
---
+++ Gustnado.Tests/Converters/EnumConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using Gustnado.Enums;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Gustnado.Tests.Converters
+{
+    public class EnumConverterTests
+    {
+        [TestCase("processing", EncodingState.Processing)]
+        [TestCase("failed", EncodingState.Failed)]
+        [TestCase("finished", EncodingState.Finished)]
+        public void FromJson(string json, EncodingState expected)
+        {
+            Assert.AreEqual(expected, JsonConvert.DeserializeObject<Foo>($"{{ State: \"{json}\"}}").State);
+        }
+
+        [TestCase(EncodingState.Processing, "processing")]
+        [TestCase(EncodingState.Failed, "failed")]
+        [TestCase(EncodingState.Finished, "finished")]
+        public void ToJson(EncodingState state, string expected)
+        {
+            Assert.AreEqual($"{{\"State\":\"{expected}\"}}", JsonConvert.SerializeObject(new Foo { State = state }));
+        }
+
+        private class Foo
+        {
+            public EncodingState State { get; set; }
+        }
+    }
+}
