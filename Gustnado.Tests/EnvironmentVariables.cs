330c00ff3f7becdb7b46681f52d37b5686bb8cc1
Itamar Amith <itamaram@gmail.com>
2017-01-26 11:24:57 +1100

    Add configuration via envvars

---
+++ Gustnado.Tests/EnvironmentVariables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gustnado.Tests
+{
+    public static class EnvironmentVariables
+    {
+        public static string ClientId { get; } = Get("Sharknado.Tests.ClientId");
+        public static string ClientSecret { get; } = Get("Sharknado.Tests.ClientSecret");
+        public static string Username { get; } = Get("Sharknado.Tests.Username");
+        public static string Password { get; } = Get("Sharknado.Tests.Password");
+
+        private static string Get(string variable) => Environment.GetEnvironmentVariable(variable);
+    }
+}
---
+++ Gustnado.Tests/envvars.bat
@@ -0,0 +1,6 @@
+@echo off
+
+setx Sharknado.Tests.ClientId ######
+setx Sharknado.Tests.ClientSecret ######
+setx Sharknado.Tests.Username ######
+setx Sharknado.Tests.Password ######
